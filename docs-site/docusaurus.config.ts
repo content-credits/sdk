475bc360d671dca1809a47f25d8ac38e324947f4
Kathan Patel <kathan565@gmail.com>
2026-03-24 12:30:00 -0600

    Use real favicon from frontend, fix dashboard URL

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -6,6 +6,10 @@ const config: Config = {
   title: 'Content Credits SDK',
   tagline: 'Drop-in paywall and comments for any website',
   favicon: 'img/favicon.ico',
+  headTags: [
+    { tagName: 'link', attributes: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/favicon-32x32.png' } },
+    { tagName: 'link', attributes: { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/favicon-16x16.png' } },
+  ],
 
   url: 'https://docs.contentcredits.com',
   baseUrl: '/',
